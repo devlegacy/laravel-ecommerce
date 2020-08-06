24273f72a7529b0229b087c6f4d93cd2fb985f5f
Samuel R <samuel_-_rojas@hotmail.com>
2020-08-06 03:04:10 -0500

    Review resources

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Product;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Resources\Review\ReviewCollection;
 
 class ReviewController extends Controller
 {
-    //
+    public function index(Product $product)
+    {
+        return new ReviewCollection($product->reviews);
+    }
 }
