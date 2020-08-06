930b70e4237ad092d3882f5b1d2e88e389ef671c
Samuel R <samuel_-_rojas@hotmail.com>
2020-08-06 02:12:13 -0500

    Modify ProductResource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,15 +15,16 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'               => $this->id,
-            'name'             => $this->name,
-            'description'      => $this->description,
-            'price'            => $this->price,
-            'discount'         => $this->discount,
-            'stock'            => $this->stock === 0 ? 'Out of stock' : $this->stock,
-            'image'            => $this->image,
-            'rating'           => $this->reviews->count() > 0 ? bcdiv($this->reviews->sum('stars'), $this->reviews->count(), 2) : 'No rating yet',
-            'href'             => [
+            'id'                 => $this->id,
+            'name'               => $this->name,
+            'description'        => $this->description,
+            'price'              => $this->price,
+            'percentageDiscount' => $this->discount, // percentage discount
+            'discountPrice'      => bcmul(bcsub(1, bcdiv($this->discount, 100, 2), 2), $this->price, 2),
+            'stock'              => $this->stock === 0 ? 'Out of stock' : $this->stock,
+            'image'              => $this->image,
+            'rating'             => $this->reviews->count() > 0 ? bcdiv($this->reviews->sum('stars'), $this->reviews->count(), 2) : 'No rating yet',
+            'href'               => [
                 'reviews' => route('reviews.index', $this->id),
             ],
         ];
