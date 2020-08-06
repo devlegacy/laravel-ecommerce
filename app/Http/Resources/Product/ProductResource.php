84466e5470f162f83cd85696d6153080b66b16dd
Samuel R <samuel_-_rojas@hotmail.com>
2020-08-06 02:52:07 -0500

    Product resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -14,19 +14,28 @@ class ProductResource extends JsonResource
      */
     public function toArray($request)
     {
+        $discountPrice = bcmul(bcsub(1, bcdiv($this->discount, 100, 2), 2), $this->price, 2);
+
         return [
             'id'                 => $this->id,
             'name'               => $this->name,
-            'description'        => $this->description,
-            'price'              => $this->price,
-            'percentageDiscount' => $this->discount, // percentage discount
-            'discountPrice'      => bcmul(bcsub(1, bcdiv($this->discount, 100, 2), 2), $this->price, 2),
-            'stock'              => $this->stock === 0 ? 'Out of stock' : $this->stock,
-            'image'              => $this->image,
+            'description'        => $this->routeIs('products.show', $this->description),
+            'price'              => $this->routeIs('products.show', $this->price),
+            'percentageDiscount' => $this->routeIs('products.show', $this->discount), // percentage discount
+            'discountPrice'      => $discountPrice,
+            'discount'           => $this->routeIs('products.show', bcsub($this->price, $discountPrice, 2)),
+            'stock'              => $this->routeIs('products.show', $this->stock === 0 ? 'Out of stock' : $this->stock),
+            'image'              => $this->routeIs('products.show', $this->image),
             'rating'             => $this->reviews->count() > 0 ? bcdiv($this->reviews->sum('stars'), $this->reviews->count(), 2) : 'No rating yet',
             'href'               => [
-                'reviews' => route('reviews.index', $this->id),
+                'reviews' => $this->routeIs('products.show', route('reviews.index', $this->id)),
+                'link'    => $this->routeIs('products.index', route('products.show', $this->id)),
             ],
         ];
+    }
+
+    private function routeIs(string $name, $data)
+    {
+        return $this->when(request()->routeIs($name), $data);
     }
 }
